a348aae1a7ec70229c644cc4baddba43a692a9a5
Wonjun Lee <mymamyma@gmail.com>
2024-04-29 12:42:22 -0500

    updated

--- train.py
+++ train.py
@@ -10,7 +10,6 @@ import numpy as np
 import math
 import torch
 import numpy as np
-from torch.nn.functional import pad
 from src.Phi import *
 import lib.toy_data as toy_data
 import matplotlib.pyplot as plt
@@ -21,8 +20,8 @@ parser.add_argument(
     type=str, default='8gaussians'
 )
 
-parser.add_argument("--nt"    , type=int, default=1, help="number of time steps")
-parser.add_argument("--nt_val", type=int, default=1, help="number of time steps for validation")
+parser.add_argument("--nt"    , type=int, default=10, help="number of time steps")
+parser.add_argument("--nt_val", type=int, default=10, help="number of time steps for validation")
 parser.add_argument("--tau", type=float, default=0.001, help="the time stepsize for the outer JKO iteration")
 parser.add_argument('--alph'  , type=str, default='1.0,1.0,0.0')
 parser.add_argument('--m'     , type=int, default=128)
@@ -37,7 +36,6 @@ parser.add_argument("--drop_freq"   , type=int  , default=500, help="how often t
 parser.add_argument('--weight_decay', type=float, default=0.0)
 parser.add_argument('--lr_drop'     , type=float, default=1.00001)
 parser.add_argument('--optim'       , type=str  , default='adam', choices=['adam'])
-parser.add_argument('--prec'        , type=str  , default='single', choices=['single','double'], help="single or double precision")
 
 parser.add_argument('--save'    , type=str, default='experiments')
 parser.add_argument('--viz_freq', type=int, default=200)
@@ -51,22 +49,18 @@ args = parser.parse_args()
 
 args.alph = [float(item) for item in args.alph.split(',')]
 
-# get precision type
-if args.prec =='double':
-    prec = torch.float64
-else:
-    prec = torch.float32
-
 # get timestamp for saving models
 start_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 
 os.makedirs(args.save, exist_ok=True)
 os.makedirs(f"{args.save}/figs", exist_ok=True)
 
-device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+cuda = True if torch.cuda.is_available() else False
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
 def compute_loss_gf(net, x, nt, tau, n_tau, net_list, rho, z):
-    Jc , cs = OTFlowProblemGradientFlowsPorous(x, rho, net, [0,1], nt=nt, tau=tau, n_tau=n_tau, net_list=net_list, stepper="rk4", alph=net.alph, z=z)
+    Jc , cs = OTFlowProblemGradientFlowsPorous(x, rho, net, [0,1], nt=nt, tau=tau, n_tau=n_tau, net_list=net_list, stepper="rk1", alph=net.alph, z=z)
     return Jc, cs
 
 def plot_scatter(x_next, args, n_tau, sPath):
@@ -91,9 +85,7 @@ def plot_scatter_color(x_next, args, n_tau, color, sPath):
     circle1 = plt.Circle((0, 0), r, color='g', fill=False)
     
     fig, ax = plt.subplots(1,1,figsize=(4,4))
-
-    x_next = x_next.numpy()
-    ax.scatter(x_next[:,0], x_next[:,1], c=color, marker='.')
+    ax.scatter(x_next.cpu()[:,0], x_next.cpu()[:,1], c=color.cpu(), marker='.')
     ax.set_xlim([-1.4,1.4])
     ax.set_ylim([-1.4,1.4])
     
@@ -106,10 +98,10 @@ def plot_scatter_color(x_next, args, n_tau, color, sPath):
 
 
 def multiple_exp(Tx):
-    pos1= torch.tensor([[2.0,-2.0]])
-    pos2= torch.tensor([[-2.0,2.0]])
-    pos3= torch.tensor([[2.0,2.0]])
-    pos4= torch.tensor([[-2.0,-2.0]])
+    pos1= Tensor([[2.0,-2.0]])
+    pos2= Tensor([[-2.0,2.0]])
+    pos3= Tensor([[2.0,2.0]])
+    pos4= Tensor([[-2.0,-2.0]])
     return 1/(2*np.pi) * ( torch.exp(-torch.sum((Tx-pos1)**2, dim=1)/(2 * 0.5**2)) 
                          + torch.exp(-torch.sum((Tx-pos2)**2, dim=1)/(2 * 0.5**2))  
                          + torch.exp(-torch.sum((Tx-pos3)**2, dim=1)/(2 * 0.5**2))
@@ -236,8 +228,7 @@ def stepRK1(odefun, z, Phi, alph, t0, t1):
     :param t1:     float, end time
     :return: tensor nex-by-d+4, features at time t1
     """
-    z += (t1 - t0) * odefun(z, t0, Phi, alph=alph)
-    return z
+    return z + (t1 - t0) * odefun(z, t0, Phi, alph=alph)
 
 def odefun(x, t, net, alph=[1.0,1.0,1.0]):
     """
@@ -268,10 +259,6 @@ def odefun(x, t, net, alph=[1.0,1.0,1.0]):
     return torch.cat( (dx,dl,dv,dr) , 1  )
 
 if __name__ == '__main__':
-
-    torch.set_default_dtype(prec)
-    cvt = lambda x: x.type(prec).to(device, non_blocking=True)
-
     # neural network for the potential function Phi
     d      = 2
     alph   = args.alph
@@ -280,9 +267,8 @@ if __name__ == '__main__':
     tau    = args.tau
     nTh    = args.nTh
     m      = args.m
-    net = Phi(nTh=nTh, m=args.m, d=d, alph=alph)
-    net = net.to(prec).to(device)
-    n_tau = args.n_tau
+    n_tau  = args.n_tau
+    net = Phi(nTh=nTh, m=args.m, d=d, alph=alph).to(device)
 
 
     b1 = 0.5
@@ -294,35 +280,24 @@ if __name__ == '__main__':
         filename = os.path.join(args.save, '{:}_alph{:}_{:}_m{:}_n{}_checkpt.pth'.format(args.data,int(alph[1]),int(alph[2]),m,i))
         print(f"loading {filename}")
         checkpt  = torch.load(filename, map_location=lambda storage, loc: storage)
-        net1     = Phi(nTh=nTh, m=m, d=d, alph=alph)
-        prec1    = checkpt['state_dict']['A'].dtype
-        net1     = net1.to(prec1).to(device)
+        net1     = Phi(nTh=nTh, m=m, d=d, alph=alph).to(device)
         net1.load_state_dict(checkpt['state_dict'])
         net_list.append(net1)
 
         if i == n_tau - 1:
             net.load_state_dict(checkpt['state_dict'])
-            x0 = checkpt['x0']
-            rho = checkpt['rho']
-            energy_list = checkpt['energy']
-            energy_list.append(0)
-            error_list = checkpt['error']
-            error_list.append(0)
-
 
     # if it is the first outer iteration then sample random points
     if n_tau == 0:
         x0,rho = toy_data.inf_train_gen(args.data, batch_size=args.batch_size, dim=d)
         print(x0.shape, rho.shape)
-        x0 = cvt(torch.from_numpy(x0))
-        rho = cvt(torch.from_numpy(rho))
-        energy_list = [0]
-        error_list = [0]
+        x0 = Tensor(x0)
+        rho = Tensor(rho)
 
         # for plotting rho(t=0)
         x0plot,rhoplot = toy_data.inf_train_gen(args.data, batch_size=1000, dim=d)
-        x0plot = cvt(torch.from_numpy(x0plot))
-        rhoplot = cvt(torch.from_numpy(rhoplot))
+        x0plot = Tensor(x0plot)
+        rhoplot = Tensor(rhoplot)
         plot_scatter_color(x0plot, args, n_tau-1, rhoplot, sPath = os.path.join(args.save, 'figs', f"rho_{n_tau}.png"))
         del x0plot
         del rhoplot
@@ -345,14 +320,13 @@ if __name__ == '__main__':
 
     # setup data [nSamples, d]
     # use one batch as the entire data set
-
-
     x0val,rhoval = toy_data.inf_train_gen(args.data, batch_size=args.val_batch_size, dim=d)
-    x0val = cvt(torch.from_numpy(x0val))
+    x0val  = Tensor(x0val)
+    rhoval = Tensor(rhoval)
 
     log_msg = (
-        '{:5s}  {:6s}   {:9s}  {:9s}  {:9s}  {:9s}  {:9s}      {:9s}  {:9s}  {:9s}  {:9s}  '.format(
-            'iter', ' time','loss', 'L (L_2)', 'C (loss)', 'R (HJB)', 'rel loss', 'valLoss', 'valL', 'valC', 'valR'
+        '{:5s}  {:6s}   {:9s}  {:9s}  {:9s}  '.format(
+            'iter', ' time','loss', 'L (L_2)', 'C (loss)'
         )
     )
     print(log_msg)
@@ -361,48 +335,40 @@ if __name__ == '__main__':
 
     previous_loss = 1
 
-    ccc = True # for plotting in between
-
     global_loss = 1
 
     best_params = net.state_dict()
     time_meter = [time.time()]
 
     for itr in range(0, args.niters + 1):
-        # train sampling
-        if itr % 100 == 0:
+        # resampling data from the initial distribution
+        if itr % args.sample_freq == 0:
             x0,rho = toy_data.inf_train_gen(args.data, batch_size=args.batch_size)
-            x0 = cvt(torch.from_numpy(x0))
-            rho = cvt(torch.from_numpy(rho))
-
+            x0 = Tensor(x0)
+            rho = Tensor(rho)
             h = 1.0 / nt
-
             # initialize "hidden" vector to propogate with all the additional dimensions for all the ODEs
-            # z = pad(x0, (0, 3, 0, 0), value=0)
             z = torch.zeros((x0.shape[0], d+3)) ; z[:,:d] = x0
+            z = z.to(device)
             # get the inital density from net_list. Given an initial density at t=0, iterate through n_tau - 1
             rho_next = rho
             with torch.no_grad(): 
                 for n in range(n_tau):
                     tk = 0
                     for k in range(nt):
-                        z = stepRK4(odefun, z, net_list[n], alph, tk, tk + h)
+                        z = stepRK1(odefun, z, net_list[n], alph, tk, tk + h)
                         tk += h
-                        rho_next = rho_next / torch.exp(z[:,d]) + 1e-5
-                    # z = pad(z[:,0:d], (0,3,0,0), value=0)
+                        rho_next = rho_next / (torch.exp(z[:,d]) + 1e-4)
                     z[:,d:] = 0
 
         optim.zero_grad()
-        loss, costs  = compute_loss_gf(net, x0, nt=nt, tau=tau, n_tau=n_tau, net_list=net_list, rho=rho, z=z)
+        loss, costs  = compute_loss_gf(net, x0, nt=nt, tau=tau, n_tau=n_tau, net_list=net_list, rho=rho_next, z=z)
         loss.backward()
         optim.step()
 
         time_meter.append(time.time())
 
         # stopping condition
-        tol = 1e-10
-        rel_loss = abs(previous_loss - loss)/abs(previous_loss)
-        # if rel_loss < tol or itr == args.niters:
         if itr == args.niters:
             # best_loss   = test_loss.item()
             # best_costs = test_costs
@@ -411,69 +377,31 @@ if __name__ == '__main__':
                 best_costs  = costs
                 best_params = net.state_dict()
 
-                x0val,rhoval = toy_data.inf_train_gen(args.data, batch_size=1000, dim=d)
-                x0val  = cvt(torch.from_numpy(x0val))
-                rhoval = cvt(torch.from_numpy(rhoval))
-
                 # compute samples at the next time step
                 x_next, rho_next = get_samples_next_time_step_including_det(x0val, rhoval, net, net_list, nt, n_tau)
-
-                mean = torch.mean(x_next, dim=0)
-                var  = torch.var(x_next)
-
-                print(f"mean = {mean} variance = {var}")
                 plot_scatter_color(x_next, args, n_tau, rho_next, sPath = os.path.join(args.save, 'figs', f"rho_{n_tau+1}.png"))
 
-                t0  = 0.001
-                tau = args.tau
-                t = tau * (n_tau+1)
-
-                m_power = 2
-                A = (4 * np.pi * m_power * (t+t0)) ** ((1-m_power)/m_power)
-                B = (m_power-1)/(4*m_power**2*(t+t0))
-                rho_next_exact = np.zeros((x_next.shape[0]))
-                for i,x in enumerate(x_next):
-                    xnorm2 = torch.sum(x**2)
-                    rho_next_exact[i] = max(A - B * xnorm2, 0)**(1/(m_power-1))
-
-                rho_next = rho_next.numpy()
-                energy_list[-1] = costs[1]
-                error_list[-1]  = np.mean((rho_next - rho_next_exact)**2)
-                np.savetxt("error.csv", np.array(error_list), delimiter=",")
-                
-                do_save = True # tmp var for saving the data
-                if do_save == True:
-                    torch.save({
-                        'args': args,
-                        'state_dict': best_params,
-                        'x0': x_next,
-                        'rho': rho_next,
-                        'error': error_list,
-                        'energy': energy_list,
-                    }, os.path.join(args.save, '{:}_alph{:}_{:}_m{:}_n{}_checkpt.pth'.format(args.data,int(alph[1]),int(alph[2]),m,n_tau)))
-                    print(f"breaking rel loss={rel_loss}")
-                    print(f"error saved in {os.path.join(args.save, '{:}_alph{:}_{:}_m{:}_n{}_checkpt.pth'.format(args.data,int(alph[1]),int(alph[2]),m,n_tau))}")
+                torch.save({
+                    'args': args,
+                    'state_dict': best_params,
+                }, os.path.join(args.save, '{:}_alph{:}_{:}_m{:}_n{}_checkpt.pth'.format(args.data,int(alph[1]),int(alph[2]),m,n_tau)))
+                print(f"data saved in {os.path.join(args.save, '{:}_alph{:}_{:}_m{:}_n{}_checkpt.pth'.format(args.data,int(alph[1]),int(alph[2]),m,n_tau))}")
             break
 
         previous_loss = loss
 
-        log_message = (
-            '{:05d}  {:6.3f}   {:9.3e}  {:9.3e}  {:9.3e}  {:9.3e}  {:9.3e}'.format(
-                itr, time_meter[-1] - time_meter[-2] , loss, costs[0], costs[1], costs[2], rel_loss
-            )
-        )
-
         if loss.item() < global_loss:
             global_loss = loss.item()
             best_params = net.state_dict()
-
 
         if itr % 1000 == 0:
             net.load_state_dict(best_params)
 
         # create plots
         if itr % args.viz_freq == 0:
-            print(log_message) # print iteration
+            print('{:05d}  {:6.3f}   {:9.3e}  {:9.3e}  {:9.3e}  '.format(
+                itr, time_meter[-1] - time_meter[-2] , loss, costs[0], costs[1]
+            ))
             with torch.no_grad():
                 # compute samples at the next time step
                 x_next, rho_next = get_samples_next_time_step_including_det(x0val, rhoval, net, net_list, nt, n_tau)
@@ -481,7 +409,10 @@ if __name__ == '__main__':
                 var  = torch.var(x_next)
                 x_next, rho_next = get_samples_next_time_step_including_det(x0val, rhoval, net, net_list, nt, n_tau)
                 plot_scatter_color(x_next, args, n_tau, rho_next, sPath = os.path.join(args.save, 'figs', f'n_tau_{n_tau}_itr_{itr}.png'))
-
+            torch.save({
+                        'args': args,
+                        'state_dict': best_params,
+                    }, os.path.join(args.save, '{:}_alph{:}_{:}_m{:}_n{}_checkpt.pth'.format(args.data,int(alph[1]),int(alph[2]),m,n_tau)))
         # shrink step size
         if itr % args.drop_freq == 0:
             for p in optim.param_groups:
@@ -491,8 +422,3 @@ if __name__ == '__main__':
 
     print("Training Time: {:} seconds".format(time_meter[-1] - time_meter[0]))
     print('Training has finished.  ' + '{:}_alph{:}_{:}_m{:}_checkpt.pth'.format(args.data,int(alph[1]),int(alph[2]),m))
-
-
-
-
-
